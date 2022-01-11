6b57b1379e7fe90d7fe901d67cf96068e2d8546b
Desiree Mendes <desireefrost@live.ca>
2022-01-11 17:17:15 -0500

    Add Readme description and screenshot

---
+++ readme.md
@@ -0,0 +1,31 @@
+# Keeper
+React notes keeper app
+
+Created from the Udemy Course: The Complete 2020 Web Development Bootcamp
+By Dr. Angela Yu
+
+## Screenshots 
+
+### A place to store, create, and delete your notes
+!["Notes Display"]()
+
+## Setup
+Install dependencies with `npm install`.
+
+
+## Dependencies
+```sh
+- React
+```
+
+## Setup
+
+Install dependencies with `npm install`.
+
+## Running Server
+
+```sh
+npm start
+```
+
+
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,7 +4,6 @@ import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
 
-
 function App() {
   const [notes, setNotes] = useState([]);
 
@@ -15,23 +14,26 @@ function App() {
   }
 
   function deleteNote(id) {
-setNotes(prevNotes => {
-  return prevNotes.filter((noteItem, index) => {
-    return index !== id;
-  })
-})
+    setNotes((prevNotes) => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      });
+    });
   }
   return (
     <div>
       <Header />
       <CreateArea onAdd={addNote} />
       {notes.map((noteItem, index) => {
-        return <Note 
-        key={index}
-        id={index}
-        title={noteItem.content} 
-        content={noteItem.content} 
-        onDelete={deleteNote} />;
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={noteItem.content}
+            content={noteItem.content}
+            onDelete={deleteNote}
+          />
+        );
       })}
       <Footer />
     </div>
